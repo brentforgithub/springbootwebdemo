c0bbde1b4e4e05825738fb0e982213c0ca501622
brent <121913371@qq.com>
2020-12-28 15:19:44 +0800

    druid连接池，sql打印log

--- src/main/java/com/brent/demo/controller/DemoController.java
+++ src/main/java/com/brent/demo/controller/DemoController.java
@@ -1,9 +1,12 @@
 package com.brent.demo.controller;
 
 import com.brent.demo.config.DemoConfig;
+import com.brent.demo.dao.NameDemoMapper;
+import com.brent.demo.mode.po.NameDemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -13,10 +16,19 @@ public class DemoController {
     @Autowired
     private DemoConfig demoConfig;
 
+    @Autowired
+    private NameDemoMapper nameDemoMapper;
+
     @RequestMapping("/hello")
     @ResponseBody
     public String hello() {
         return "Hello World " +  demoConfig.getProperty();
+    }
+
+    @RequestMapping("/showDemo")
+    @ResponseBody
+    public NameDemo showDemo(@RequestParam Integer id) {
+        return nameDemoMapper.selectByPrimaryKey(id);
     }
 
 
